c8e1b3112d1c3c4bf5de3e240c06043737a389d8
Samuel Vasconcelos <samuel.moreira@luizalabs.com>
2022-01-26 08:58:38 -0300

    feat: add migrations

--- migrations/20220122164728_transactions.sql
+++ migrations/20220122164728_transactions.sql
@@ -1,7 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS t_transactions (
-   
+   id int AUTO_INCREMENT PRIMARY KEY,
+   date DATE,
+   amount NUMERIC(15,2) NOT NULL,
+   observation VARCHAR(120),
+   protocol VARCHAR(225),
+   status VARCHAR(12),
 );
 -- +goose StatementEnd
 
